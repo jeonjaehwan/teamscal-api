f394478505a48510273f03ff691dc27321ed77dd
전재환 <jeonjaehwan@jeonjaehwan-ui-MacBookAir.local>
2026-05-07 20:02:51 +0900

    first commit

---
+++ src/main/java/com/teamscal/api/TeamscalApiApplication.java
@@ -0,0 +1,13 @@
+package com.teamscal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamscalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamscalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamscal/api/TeamscalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamscal.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamscalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
